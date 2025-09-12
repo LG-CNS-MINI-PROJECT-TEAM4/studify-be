f586bb65ef5cb21891a5e8744f3e6f9d8c26e560
MinJaeSon <mjsonsj@gmail.com>
2025-09-12 16:07:35 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/lgcns/studify_be/StudifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.studify_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudifyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudifyBeApplication.class, args);
+	}
+
+}
