7d13145e35cf90c227060f4b85d5e14aa585e29e
hsuncho <hsuncho@naver.com>
2025-09-15 17:40:48 +0900

    test: Post update, delete 테스트

--- src/test/java/com/lgcns/studify_be/PostEntityTests.java
+++ src/test/java/com/lgcns/studify_be/PostEntityTests.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lgcns.studify_be.post.domain.dto.PostRequestDTO;
@@ -14,14 +13,15 @@ import com.lgcns.studify_be.post.domain.entity.PostEntity;
 import com.lgcns.studify_be.post.repository.PostRepository;
 
 @SpringBootTest
+@Transactional
 public class PostEntityTests {
     
+    // yaml 변경 필요
+
     @Autowired
     private PostRepository postRepository;
 
     @Test
-    @Transactional
-    @Commit
     public void createPost() {
         PostRequestDTO request = PostRequestDTO.builder()
                                                 .title("test-title1")
@@ -40,6 +40,44 @@ public class PostEntityTests {
         List<PostEntity> postList = postRepository.findAll();
         System.out.println(">>> 포스트 수 : " + postList.size());
         postList.forEach(System.out::println);
+    }
+
+    @Test
+    public void updatePost() {
+        PostEntity post = postRepository.save(
+            PostRequestDTO.builder()
+                        .title("before-update-test-title1")
+                        .content("before-update-test-content1")
+                        .category(Category.PROJECT)
+                        .build()
+                        .toEntity()
+        );
+
+        System.out.println(">>> 저장 데이터 : " + postRepository.findAll());
+        
+        post.setTitle("after-update-test-title1");
+        postRepository.save(post);
+        
+        PostEntity updatedPost = postRepository.findById(post.getPostId()).get();
+        System.out.println(">>> 수정 결과 : " + updatedPost);
+    }
+    
+    @Test
+    public void deletePost() {
+        PostEntity post = postRepository.save(
+            PostRequestDTO.builder()
+            .title("delete-test-title1")
+            .content("delete-test-content1")
+            .category(Category.PROJECT)
+            .build()
+            .toEntity()
+        );
+            
+        System.out.println(">>> 저장 데이터 : " + postRepository.findAll());
+        
+        postRepository.delete(post);
+        System.out.println(">>> 삭제 결과: " + postRepository.existsById(post.getPostId()));
+        System.out.println(">>> 저장 데이터 : " + postRepository.findAll());
     }
 
 }
